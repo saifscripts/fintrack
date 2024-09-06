950c3b0176083de0b28e4dc85749e7d05827ef44
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-09-06 21:05:58 +0600

    update twitter options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -23,6 +23,7 @@ const handler = NextAuth({
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID!,
       clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+      version: '2.0', // opt-in to Twitter OAuth 2.0
     }),
   ],
 });
